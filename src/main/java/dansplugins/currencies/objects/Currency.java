284a67ce8011a9a7c1d6758842479428624bea7a
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 13:34:39 -0600

    Implemented Currency class

--- src/main/java/dansplugins/currencies/objects/Currency.java
+++ src/main/java/dansplugins/currencies/objects/Currency.java
@@ -1,5 +1,45 @@
 package dansplugins.currencies.objects;
 
+import org.bukkit.Material;
+
 public class Currency {
-    // TODO: implement
+    private String name;
+    private String factionName;
+    private String material;
+    private int currencyID;
+
+    public Currency(String name, String factionName, Material material, int currencyID) {
+        this.name = name;
+        this.factionName = factionName;
+        this.material = material.name();
+        this.currencyID = currencyID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFactionName() {
+        return factionName;
+    }
+
+    public void setFactionName(String factionName) {
+        this.factionName = factionName;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getCurrencyID() {
+        return currencyID;
+    }
 }
