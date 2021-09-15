a4cb494068a04a2594d22548e3e4e74284a95688
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 13:37:06 -0600

    Created some skeleton methods in CurrencyManager.java

--- src/main/java/dansplugins/currencies/managers/CurrencyManager.java
+++ src/main/java/dansplugins/currencies/managers/CurrencyManager.java
@@ -1,5 +1,29 @@
 package dansplugins.currencies.managers;
 
+import dansplugins.factionsystem.externalapi.MF_Faction;
+import org.bukkit.Material;
+
 public class CurrencyManager {
-    // TODO: implement
+    private static CurrencyManager instance;
+
+    private CurrencyManager() {
+
+    }
+
+    public static CurrencyManager getInstance() {
+        if (instance == null) {
+            instance = new CurrencyManager();
+        }
+        return instance;
+    }
+
+    public boolean createNewCurrency(String name, MF_Faction faction, Material material) {
+        // TODO: implement;
+        return true;
+    }
+
+    private int getNewCurrencyID() {
+        // TODO: implement
+        return -1;
+    }
 }
