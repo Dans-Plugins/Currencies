cb957cc06c6f61af74411606d5394a8578311837
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 17:41:05 -0600

    Added some more console messages to MedievalFactionsIntegrator.java

--- src/main/java/dansplugins/currencies/MedievalFactionsIntegrator.java
+++ src/main/java/dansplugins/currencies/MedievalFactionsIntegrator.java
@@ -14,6 +14,8 @@ public class MedievalFactionsIntegrator {
             if (Currencies.getInstance().isDebugEnabled()) { System.out.println("[DEBUG] Medieval Factions was found successfully!"); }
             try {
                 mf_api = new MedievalFactionsAPI();
+                System.out.println("[Currencies] Using Medieval Factions External API " + getAPI().getAPIVersion());
+                System.out.println("[Currencies] Installed Version of Medieval Factions: " + getAPI().getVersion());
             }
             catch(NoClassDefFoundError e) {
                 System.out.println("[Currencies] There was a problem instantiating the Medieval Factions API. Medieval Factions might need to be updated.");
