e298610335e1b95fec9596a0a48fa90846cec6dd
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 23:41:56 -0600

    Created DepositCommand.java

---
+++ src/main/java/dansplugins/currencies/commands/DepositCommand.java
@@ -0,0 +1,12 @@
+package dansplugins.currencies.commands;
+
+import org.bukkit.command.CommandSender;
+
+public class DepositCommand {
+
+    public boolean execute(CommandSender sender, String[] args) {
+        // TODO: implement
+        return true;
+    }
+
+}
