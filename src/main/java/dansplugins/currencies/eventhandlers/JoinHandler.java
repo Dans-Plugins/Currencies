4bfcafe12305231ef3846dd08d0445cbe29b6047
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 23:29:12 -0600

    Implemented the Join Handler class

--- src/main/java/dansplugins/currencies/eventhandlers/JoinHandler.java
+++ src/main/java/dansplugins/currencies/eventhandlers/JoinHandler.java
@@ -1,5 +1,8 @@
 package dansplugins.currencies.eventhandlers;
 
+import dansplugins.currencies.data.PersistentData;
+import dansplugins.currencies.objects.Coinpurse;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,7 +15,15 @@ public class JoinHandler implements Listener {
 
         Player player = event.getPlayer();
 
+        if (PersistentData.getInstance().getCoinpurse(player.getUniqueId()) != null) {
+            // player already has a coinpurse
+            return;
+        }
 
+        // player doesn't have a coinpurse
+        Coinpurse newCoinpurse = new Coinpurse(player.getUniqueId());
+        PersistentData.getInstance().addCoinpurse(newCoinpurse);
+        player.sendMessage(ChatColor.AQUA + "You check your side and see that your coinpurse is with you. Type /c help for help."); // TODO: add config option for this message
     }
 
 }
