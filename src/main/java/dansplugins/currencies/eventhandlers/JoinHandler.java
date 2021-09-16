a3c7b738baf5f2082c2f00bd832ab8bedcd788e8
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-15 23:17:48 -0600

    Created EventRegistry.java and JoinHandler.java

---
+++ src/main/java/dansplugins/currencies/eventhandlers/JoinHandler.java
@@ -0,0 +1,14 @@
+package dansplugins.currencies.eventhandlers;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinHandler implements Listener {
+
+    @EventHandler()
+    public void handle(PlayerJoinEvent event) {
+        // TODO: implement
+    }
+
+}
