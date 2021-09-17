9c10ec0cb167dc1ce6b781d7c37c524bb5ab5ddb
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-16 23:51:22 -0600

    Made it so that when a faction is renamed, their associated currency will be updated accordingly.

---
+++ src/main/java/dansplugins/currencies/eventhandlers/FactionEventHandler.java
@@ -0,0 +1,32 @@
+package dansplugins.currencies.eventhandlers;
+
+import dansplugins.currencies.Currencies;
+import dansplugins.currencies.data.PersistentData;
+import dansplugins.currencies.managers.CurrencyManager;
+import dansplugins.currencies.objects.Currency;
+import dansplugins.factionsystem.events.FactionRenameEvent;
+import dansplugins.factionsystem.externalapi.MF_Faction;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class FactionEventHandler implements Listener {
+
+    @EventHandler()
+    public void handle(FactionRenameEvent event) {
+
+        MF_Faction faction = new MF_Faction(event.getFaction());
+
+        String oldName = event.getCurrentName();
+        String newName = event.getProposedName();
+
+        Currency currency = PersistentData.getInstance().getCurrency(faction);
+
+        if (currency == null) {
+            return;
+        }
+
+        currency.setFactionName(newName);
+        if (Currencies.getInstance().isDebugEnabled()) { System.out.println("[DEBUG] Faction Rename Event has been handled."); }
+    }
+
+}
