3100e3770adbccc685526a002af069da5142bafc
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-16 23:10:52 -0600

    Changed version to v0.6

--- src/main/java/dansplugins/currencies/Currencies.java
+++ src/main/java/dansplugins/currencies/Currencies.java
@@ -12,7 +12,7 @@ public final class Currencies extends JavaPlugin {
 
     private static Currencies instance;
 
-    private final String version = "v0.6-alpha-1";
+    private final String version = "v0.6";
 
     public static Currencies getInstance() {
         return instance;
