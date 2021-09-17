ffaab1b07cad8af105568322ae46439cd17f06f0
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-16 22:48:24 -0600

    Added the "disallowPlacement" config option

--- src/main/java/dansplugins/currencies/EventRegistry.java
+++ src/main/java/dansplugins/currencies/EventRegistry.java
@@ -1,6 +1,7 @@
 package dansplugins.currencies;
 import dansplugins.currencies.eventhandlers.CraftingHandler;
 import dansplugins.currencies.eventhandlers.JoinHandler;
+import dansplugins.currencies.eventhandlers.PlacementHandler;
 import org.bukkit.plugin.PluginManager;
 
 public class EventRegistry {
@@ -26,5 +27,6 @@ public class EventRegistry {
         // blocks and interaction
         manager.registerEvents(new JoinHandler(), mainInstance);
         manager.registerEvents(new CraftingHandler(), mainInstance);
+        manager.registerEvents(new PlacementHandler(), mainInstance);
     }
 }
