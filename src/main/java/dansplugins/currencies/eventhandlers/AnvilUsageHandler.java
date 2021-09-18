dc2bbe9c6005752416bd0e8bdae27c18d9bdf58b
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-18 17:44:31 -0600

    Altered AnvilUsageHandler.java

--- src/main/java/dansplugins/currencies/eventhandlers/AnvilUsageHandler.java
+++ src/main/java/dansplugins/currencies/eventhandlers/AnvilUsageHandler.java
@@ -1,24 +1,26 @@
 package dansplugins.currencies.eventhandlers;
 
 import dansplugins.currencies.Currencies;
+import dansplugins.currencies.managers.ConfigManager;
 import dansplugins.currencies.managers.CurrencyManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
-import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.List;
 
 public class AnvilUsageHandler implements Listener {
 
+    private boolean cooldown = false;
+
     @EventHandler()
     public void handle(PrepareAnvilEvent event) {
+        if (!ConfigManager.getInstance().getBoolean("disallowAnvilUsage")) {
+            return;
+        }
+
         if (Currencies.getInstance().isDebugEnabled()) {
             System.out.println("[DEBUG] Prepare Anvil Event is firing.");
         }
@@ -35,25 +37,24 @@ public class AnvilUsageHandler implements Listener {
                 continue;
             }
             if (CurrencyManager.getInstance().isCurrency(itemStack)) {
-                player.sendMessage(ChatColor.RED + "You can't use currencies when renaming or repairing.");
+                if (!cooldown) {
+                    player.sendMessage(ChatColor.RED + "You can't use currencies when renaming or repairing.");
+                }
                 event.setResult(null);
+                setCooldown(1);
                 return;
             }
         }
     }
 
-    private void printContents(Inventory inventory) {
-        for (ItemStack itemStack : inventory) {
-            if (itemStack != null) {
-                ItemMeta meta = itemStack.getItemMeta();
-                if (meta != null) {
-                    List<String> lore = meta.getLore();
-                    if (lore != null) {
-                        System.out.println("[ICE] " + lore);
-                    }
-                }
+    private void setCooldown(int seconds) {
+        cooldown = true;
+        Currencies.getInstance().getServer().getScheduler().runTaskLater(Currencies.getInstance(), new Runnable() {
+            @Override
+            public void run() {
+                cooldown = false;
             }
-        }
+        }, seconds * 20);
     }
 
 }
