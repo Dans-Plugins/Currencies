03996e65940dec5e74e0551ba9bd78811cd0ad04
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-21 00:32:31 -0600

    Added the /mf force rename command.

--- src/main/java/dansplugins/currencies/commands/ForceCommand.java
+++ src/main/java/dansplugins/currencies/commands/ForceCommand.java
@@ -15,7 +15,7 @@ public class ForceCommand {
     public boolean execute(CommandSender sender, String[] args) {
         
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Sub-commands: retire");
+            sender.sendMessage(ChatColor.RED + "Sub-commands: retire, rename");
             return false;
         }
 
@@ -24,6 +24,11 @@ public class ForceCommand {
 
         if (secondaryLabel.equalsIgnoreCase("retire")) {
             if (!PermissionChecker.getInstance().checkPermission(sender, "currencies.force.retire")) { return false; }
+            return forceRetire(sender, arguments);
+        }
+
+        if (secondaryLabel.equalsIgnoreCase("rename")) {
+            if (!PermissionChecker.getInstance().checkPermission(sender, "currencies.force.rename")) { return false; }
             return forceRetire(sender, arguments);
         }
 
@@ -58,6 +63,43 @@ public class ForceCommand {
 
         CurrencyManager.getInstance().retireCurrency(currency);
         sender.sendMessage(ChatColor.GREEN + "Retired.");
+        return true;
+    }
+
+    private boolean forceRename(CommandSender sender, String[] args) {
+
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Usage: /c force rename 'currencyName' 'newName'");
+            return false;
+        }
+
+        ArrayList<String> singleQuoteArgs = ArgumentParser.getInstance().getArgumentsInsideSingleQuotes(args);
+
+        if (singleQuoteArgs.size() < 2) {
+            sender.sendMessage(ChatColor.RED + "Name and new name must be designated between single quotes.");
+            return false;
+        }
+
+        String currencyName = singleQuoteArgs.get(0);
+
+        Currency currency = PersistentData.getInstance().getActiveCurrency(currencyName);
+
+        if (currency == null) {
+            sender.sendMessage(ChatColor.RED + "There are no currencies named " + currencyName);
+            return false;
+        }
+
+        String newName = singleQuoteArgs.get(1);
+
+        Currency newNameCurrency = PersistentData.getInstance().getCurrency(newName);
+
+        if (newNameCurrency != null) {
+            sender.sendMessage(ChatColor.RED + "That name is taken by an active or retired currency.");
+            return false;
+        }
+
+        currency.setName(newName);
+        sender.sendMessage(ChatColor.GREEN + "Renamed.");
         return true;
     }
 
