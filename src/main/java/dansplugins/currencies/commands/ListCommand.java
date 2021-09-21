bccc25656e0892c85ddd21ae9a9cd5d5815e5efb
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-21 11:54:29 -0600

    Altered ListCommand.java

--- src/main/java/dansplugins/currencies/commands/ListCommand.java
+++ src/main/java/dansplugins/currencies/commands/ListCommand.java
@@ -1,30 +1,34 @@
 package dansplugins.currencies.commands;
 
 import dansplugins.currencies.data.PersistentData;
+import dansplugins.currencies.utils.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class ListCommand {
 
     public boolean execute(CommandSender sender, String[] args) {
+        Logger.getInstance().log("Number of arguments: " + args.length);
         if (args.length == 0) {
             PersistentData.getInstance().sendListOfActiveCurrenciesToSender(sender);
             return true;
         }
 
         String list = args[0];
+        Logger.getInstance().log("List specified: " + list);
 
         if (list.equalsIgnoreCase("active")) {
             PersistentData.getInstance().sendListOfActiveCurrenciesToSender(sender);
+            return true;
         }
         else if (list.equalsIgnoreCase("retired")) {
             PersistentData.getInstance().sendListOfRetiredCurrenciesToSender(sender);
+            return true;
         }
         else {
             sender.sendMessage(ChatColor.RED + "Sub-commands: active, retired");
+            return false;
         }
-
-        return true;
     }
 
 }
