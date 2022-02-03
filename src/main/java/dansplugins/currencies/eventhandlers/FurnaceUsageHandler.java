82f23ed0133a1e294966fc792f6eae9f5ff55cf4
Mr-Deej <gavinpersonal9@gmail.com>
2022-02-03 14:39:10 -0600

    new method of preventing currency from being put in furnaces

--- src/main/java/dansplugins/currencies/eventhandlers/FurnaceUsageHandler.java
+++ src/main/java/dansplugins/currencies/eventhandlers/FurnaceUsageHandler.java
@@ -4,40 +4,38 @@ import dansplugins.currencies.managers.ConfigManager;
 import dansplugins.currencies.managers.CurrencyManager;
 import dansplugins.currencies.utils.Logger;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 
 public class FurnaceUsageHandler implements Listener {
 
     @EventHandler()
-    public void handle(CraftItemEvent event) {
+    public void handle(InventoryClickEvent event) {
 
         if (!ConfigManager.getInstance().getBoolean("disallowSmelting")) {
             Logger.getInstance().log("[DEBUG] Smelting with currencies is allowed.");
             return;
         }
 
-        CraftingInventory inventory = event.getInventory();
-
-        for (ItemStack itemStack : inventory.getMatrix()) {
+        for (ItemStack itemStack : event.getInventory()) {
             if (itemStack == null) {
                 continue;
             }
-            Logger.getInstance().log("Looking at item: " + itemStack.toString());
-            if (CurrencyManager.getInstance().isCurrency(itemStack)) {
-                event.getInventory().getType().equals(InventoryType.FURNACE);
-                event.getSlotType().equals(InventoryType.SlotType.CRAFTING);
-                event.getWhoClicked().sendMessage(ChatColor.RED + "You can't use currency in smelting recipes.");
+
+            if (event.getSlotType() != InventoryType.SlotType.CRAFTING) return;
+            if (event.getInventory().getType() != InventoryType.FURNACE) return;
+            if (event.getCurrentItem().equals(CurrencyManager.getInstance().isCurrency(itemStack))) {
+                Player player = (Player) event.getWhoClicked();
+                player.sendMessage(ChatColor.RED + "You can't use currency in smelting recipes.");
                 event.setCancelled(true);
-                return;
+                player.closeInventory();
+
             }
-            //We cannot prevent usage of blast furnaces as they were added in v1.14 and Currencies is using 1.13
         }
         Logger.getInstance().log("Smelting was not cancelled");
     }
-
 }
